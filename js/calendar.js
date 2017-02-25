db20404d60e9b30ed9951cd3d06a62c5e6915828
heal7 <1798800425@qq.com>
2017-02-25 20:49:58 +0800

    webapp-school-calendar

--- js/calendar.js
+++ js/calendar.js
@@ -45,6 +45,7 @@ function drawCalendar(){
 			this.daysOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
 		}
 		this.dayCount = this.daysOfMonth[this.month-1];//该月总天数
+		this.ldayCount = this.daysOfMonth[new Date(this.year+"-"+(this.month-1)+"-"+this.date).getMonth()];
 	}
 
 
@@ -55,31 +56,43 @@ function drawCalendar(){
 	}
 
 	this.fillBody = function(){   //绘制日期表格
-		if(this.month==2 && this.date>this.daysOfMonth[1]){
-			this.date = this.daysOfMonth[1];
-		}
-		else{
-			this.date = this.nowTime.getDate();
-		}
+		// if(this.month==2 && this.date>this.daysOfMonth[1]){
+		// 	this.date = this.daysOfMonth[1];
+		// }
+		// else{
+		// 	this.date = this.nowTime.getDate();
+		// }
 		
 		//填写本月信息
-		for(var i=this.offset,jday=1; i<this.offset+this.dayCount; i++){
-			var d = new Date(this.year+"-"+this.fitIE(this.month)+"-"+this.fitIE(jday));
-			var eventTitle = this.schoolevents.getTitle(d);
+		for(var i=(typeof(arguments[0])!="undefined"?arguments[0]:this.offset),jday=1;i<(typeof(arguments[0])!="undefined"?arguments[0]:this.offset)+(typeof(arguments[1])!="undefined"?arguments[1]:this.dayCount);i++){
+			var timeStr=this.year+"-"+this.fitIE(this.month)+"-"+this.fitIE(jday);
 			var today = new Date();
-			if(this.year == today.getFullYear()&& this.month == today.getMonth()+1 && jday == today.getDate()){
+			// var lunarDate=this.lunarObj.lunar_calc(solarStr).lunar_date;
+			if(this.year==today.getFullYear()&& this.month==(today.getMonth()+1)&& jday==today.getDate()){
 				body_days[i].innerHTML="<span class='today'>"+jday+"</span>";
 			}
 			else{
 				body_days[i].innerHTML="<span>"+jday+"</span>";
 			}
-			if(jday == today && eventTitle != null){
+			var d = new Date(timeStr);
+			var eventTitle = this.schoolevents.getTitle(d);
+			if(this.year == today.getFullYear()&& this.month == today.getMonth()+1 && jday == today.getDate() && eventTitle != null){
 				body_days[i].innerHTML += "<span class='event-title'>"+eventTitle+"</span>"
 			}
-			else if(jday!=today && eventTitle != null){
+			else if((this.year != today.getFullYear() || this.month != today.getMonth()+1 ||jday != today.getDate()) && eventTitle != null){
 				body_days[i].innerHTML ="<span class='eventIsShow'>"+jday+"</span>"+"<span class='event-title'>"+eventTitle+"</span>";
-			}
+			}			
 			jday++;
+		}
+		//填写下个月的日期信息
+		for(var i=(typeof(arguments[0])!="undefined"?arguments[0]:this.offset)+(typeof(arguments[1])!="undefined"?arguments[1]:this.dayCount),jday=1;i<body_days.length;i++){
+			body_days[i].innerHTML="<span>"+""+"</span>";
+			jday++;
+		}
+		//填写上个月的日期信息
+		for(var i=(typeof(arguments[0])!="undefined"?arguments[0]:this.offset)-1,jday=(typeof(arguments[2])!="undefined"?arguments[2]:this.ldayCount);i>=0;i--){
+			body_days[i].innerHTML="<span>"+""+"</span>";
+			jday--;
 		}
 	}
 	
@@ -88,35 +101,50 @@ function drawCalendar(){
 		addEvent(btn_next_month,"click",this.eventHandler.go_next_month);
 	}
 	this.eventHandler={
-		go_last_month:function(){
-			calendar.month-=1;
+		go_last_month:function(){//跳至上一个月
+			calendar.month-=1;			
 			if(calendar.month<1){
-				calendar.month = 12;
-				calendar.year -= 1;
-				calendar.isLeap(calendar.year)
+				calendar.month=12;
+				calendar.year-=1;
+				calendar.isLeap(calendar.year);
 			}
-			var monthStr = calendar.fitIE(calendar.month);
-			var dateStr = calendar.fitIE(calendar.date);
-			var today = new Date(calendar.year+'-'+monthStr+'-'+dateStr);
-			var offset = new Date(calendar.year+'-'+monthStr+'-'+'01').getDay();
-			var dayCount = calendar.daysOfMonth[calendar.month-1];
+			var monthStr=calendar.fitIE(calendar.month);
+			var monthStr1=calendar.fitIE(calendar.month-1);
+			var dateStr=calendar.fitIE(calendar.date);
+			// var today=new Date(calendar.year+"-"+monthStr+"-"+dateStr);
+			var offset=new Date(calendar.year+"-"+monthStr+"-"+"01").getDay();
+			var dayCount=calendar.daysOfMonth[calendar.month-1];
+			if(calendar.month==1){
+				var ldayCount=31;
+			}
+			else{
+				var ldayCount=calendar.daysOfMonth[new Date(calendar.year+"-"+monthStr1+"-"+dateStr).getMonth()];
+			}
 			calendar.fillNavbar();
-			calendar.fillBody(dayCount);
+			calendar.fillBody(offset,dayCount,ldayCount);
 		},
-		go_next_month:function(){
+
+		go_next_month:function(){//跳至下个月
 			calendar.month+=1;
 			if(calendar.month>12){
 				calendar.year+=1;
 				calendar.month=1;
 				calendar.isLeap(calendar.year);
 			}
-			var monthStr = calendar.fitIE(calendar.month);
-			var dateStr = calendar.fitIE(calendar.date);
-			var today = new Date(calendar.year+'-'+monthStr+'-'+dateStr);
-			var offset = new Date(calendar.year+'-'+monthStr+'-'+'01').getDay();
-			var dayCount = calendar.daysOfMonth[calendar.month-1];
+			var monthStr=calendar.fitIE(calendar.month);
+			var monthStr1=calendar.fitIE(calendar.month-1);
+			var dateStr=calendar.fitIE(calendar.date);
+			// var today=new Date(calendar.year+"-"+monthStr+"-"+dateStr);
+			var offset=new Date(calendar.year+"-"+monthStr+"-"+"01").getDay();
+			var dayCount=calendar.daysOfMonth[calendar.month-1];
+			if(calendar.month==1){
+				var ldayCount=31;
+			}
+			else{
+				var ldayCount=calendar.daysOfMonth[new Date(calendar.year+"-"+monthStr1+"-"+dateStr).getMonth()];
+			}
 			calendar.fillNavbar();
-			calendar.fillBody(dayCount);
+			calendar.fillBody(offset,dayCount,ldayCount);
 		}
 	}
 }
